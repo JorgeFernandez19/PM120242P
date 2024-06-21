1e552929f66aa3f45536b63651a791ac6b107fd4
JorgeFernandez19 <134749317+JorgeFernandez19@users.noreply.github.com>
2024-06-20 18:32:01 -0600

    Commit Inicial cambios al 20/06/2024

--- app/src/main/java/com/example/pm120242p/ActivityFot.java
+++ app/src/main/java/com/example/pm120242p/ActivityFot.java
@@ -5,7 +5,9 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.health.connect.datatypes.units.Length;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
@@ -18,9 +20,15 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class ActivityFot extends AppCompatActivity {
 
@@ -32,7 +40,7 @@ public class ActivityFot extends AppCompatActivity {
     Button btncaptura;
 
     String PathImagen;
-
+    String currentPhotoPath;
 
 
     @Override
@@ -47,7 +55,7 @@ public class ActivityFot extends AppCompatActivity {
             return insets;
         });
 
-        ObejactoImagen =  (ImageView) findViewById(R.id.imageView);
+        ObejactoImagen = (ImageView) findViewById(R.id.imageView);
         btncaptura = (Button) findViewById(R.id.btntakefoto);
 
         btncaptura.setOnClickListener(new View.OnClickListener() {
@@ -62,43 +70,33 @@ public class ActivityFot extends AppCompatActivity {
 
     }
 
-    private void  Permisos ()
-    {
-if(ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) !=
-        PackageManager.PERMISSION_GRANTED)
-{
-    ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA},peticion_acceso_camara );
-}
-else
-{
-    TomarFoto();
-}
+    private void Permisos() {
+        if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) !=
+                PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, peticion_acceso_camara);
+        } else {
+            TomarFoto();
+        }
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
-        if (requestCode ==peticion_acceso_camara)
-        {
-            if (grantResults.length >0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
-            {
+        if (requestCode == peticion_acceso_camara) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 TomarFoto();
-            }
-            else
-            {
+            } else {
                 Toast.makeText(getApplicationContext(), "Acceso Denegado", Toast.LENGTH_LONG).show();
             }
         }
     }
 
-    private void TomarFoto()
-    {
+    private void TomarFoto() {
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
-        if (intent.resolveActivity(getPackageManager())!=null)
-        {
-            startActivityForResult(intent,peticion_captura_imagen );
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent, peticion_captura_imagen);
         }
     }
 
@@ -106,12 +104,50 @@ else
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if(requestCode == peticion_captura_imagen)
-        {
+        if (requestCode == peticion_captura_imagen) {
             Bundle extras = data.getExtras();
             Bitmap imagen = (Bitmap) extras.get("data");
             ObejactoImagen.setImageBitmap(imagen);
 
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.pmo120232p.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, peticion_captura_imagen);
+            }
         }
     }
 }
